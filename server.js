f1a44d1e81399f9e22744ff9f8d46b8b5921f171
Beza G. Tassew <getbez@gmail.com>
2023-11-25 10:23:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-
+const DAY_OF_WEEK = process.env.DAY_OF_WEEK
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`${DAY_OF_WEEK} rocks!`);
     await sleep(5000);
   }
 }
